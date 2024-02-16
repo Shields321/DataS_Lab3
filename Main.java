19609ceab5fd5b7cd367cf47f74d7c0f4ea6a640
Shields321 <106932598+Shields321@users.noreply.github.com>
2024-02-15 19:01:31 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,34 +1,38 @@
 package datas_lab3;
+
 /*
 Group members
 Alessio Barranca and Dylan Kokilepersaud
 */
 public class DataS_Lab3 {
 
+    public static int num =20;
+    public static int count =0;
     public static void main(String[] args) {
-        MyLinkedList<Object> list = new MyLinkedList<>();
-        list.addFirst(2);
-        list.addFirst(4);
-        list.delFirst();
-        list.addFirst(3);
-        list.delFirst();
-        list.addFirst(10);
-        list.addFirst(11);
-        list.addFirst(9);
-        list.addFirst(-4);
-        list.addFirst(-5);
-        list.addFirst(20);
+        MyLinkedList<Object> list = new MyLinkedList<>();  
+
+        System.out.println(list.isEmpty());
+        for(int i =0;i<=30;i++){
+            list.addFirst(nextRandom());
+        }      
+        System.out.println("Insertion the value of 87 ");
+        list.insert(5, 87);        
+        System.out.println(list.toString());
+        System.out.println(list.isEmpty());
+        System.out.println("Largest value in the list "+list.getLargest());
+        System.out.println("Amount of negative numbers in the list = " +list.countNegative());        
         list.sort();
-       
+        System.out.println(list.toString());      
+    }    
+    public static int nextRandom() {
         
-        System.out.println(list.Search(-5));
-        System.out.println("Ammount of negative numbers is " + list.countNegative());
-        System.out.println(list.getLargest());
-        System.out.println(list.toString());
-        list.insert(2, -9);
-        list.sort();
-        System.out.println(list.toString());
-       
+        if(count%2  == 0){
+            num = num+3;
+        }
+        else{            
+            num = num+2;
+        }
+        return num;
     }
     
 }
